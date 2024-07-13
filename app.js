0cf44417f3d48dd158cf32dd7bd531b8187ad7b1
moviesworldbyshanie <127875416+moviesworldbyshanie@users.noreply.github.com>
2024-07-13 21:35:56 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let productimg= document.getElementById("productimg");
+let btn= document.getElementsByClassName("btn");
+
+btn[0].onclick = function() {
+    productimg.src= "images/image1.png";
+    for(bt of btn){
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
+
+btn[1].onclick = function() {
+    productimg.src= "images/image2.png";
+    for(bt of btn){
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
+
+btn[2].onclick = function() {
+    productimg.src= "images/image3.png";
+    for(bt of btn){
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
